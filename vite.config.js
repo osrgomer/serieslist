d4ba9d48ccdd12da5f93665c4a2b14abb4024c05
Omer <omersr12@gmail.com>
2025-12-26 21:11:50 +0000

    test

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // This tells Vite to use "relative paths" so Hostinger can find the files 
+  // regardless of which folder they are placed in.
+  base: './', 
 })
